f7ecabc4abf8346104c9bceb27eeded581d840d7
VodeniZeko <salexy.es@gmail.com>
2020-06-04 13:19:42 -0700

    game

---
+++ src/items.py
@@ -0,0 +1,7 @@
+from item import Item
+
+
+items = [
+    Item("lighter",
+         "A mechanical device for lighting a cigarette, cigar, or pipe.", 0),
+]
--- src/room.py
+++ src/room.py
@@ -1,2 +1,29 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from items import items
+
+
+class Room:
+    def __init__(self, name, description, id, item_ids=[]):
+        self.name = name
+        self.description = description
+        self.items = [items[item_id] for item_id in item_ids]
+        self.id = id
+
+    def search(self):
+        if len(self.items) == 0:
+            print("*** You search the area and found nothing: ***")
+        else:
+            print("You search the area and find:")
+            for item in self.items:
+                print(item.name)
+
+    def takeitem(self, itemname):
+        if next(item for item in self.items if item.name.lower() == itemname):
+            item = next(
+                item for item in self.items if item.name.lower() == itemname)
+            self.items.remove(item)
+            return item
+        else:
+            return False
